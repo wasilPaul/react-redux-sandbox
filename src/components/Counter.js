a7583b420108510737d9aca13f5ca77736341c70
Pawel Wasil <wasil.paul@gmail.com>
2018-05-15 11:43:25 +0200

    Dispatch Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,20 +1,30 @@
 import React from 'react'
-
+import { inc, dec, multi, divi } from '../state/counter'
 import { connect } from 'react-redux'
 import { RaisedButton } from 'material-ui';
 
 const Counter = (props) =>
     <div>
         <h2> {props.counterValue} </h2>
-        <RaisedButton label={'-'} primary={true} />
-        <RaisedButton label={'+'} secondary={true} />
+        <RaisedButton label={'-'} primary={true} onClick={props.onDecHandler} />
+        <RaisedButton label={'+'} secondary={true} onClick={props.onIncHandler} />
+        <br />
+        <RaisedButton label={'x 2'} secondary={true} onClick={props.onMultiHandler} />
+        <RaisedButton label={'/ 2'} primary={true} onClick={props.onDiviHandler} />
     </div>
 
 const mapStateToProps = state => ({
     counterValue: state.counter
-}) 
+})
+
+const mapDispatchToProps = dispatch => ({
+    onDecHandler: () => dispatch(dec()), // do tego prpsa przypisz funkcje ktra wywola funkcje
+    onIncHandler: () => dispatch(inc()),
+    onMultiHandler: () => dispatch(multi(2)),
+    onDiviHandler: () => dispatch(divi(2)),
+})
 
 export default connect(
-    mapStateToProps,    
-    //
+    mapStateToProps,
+    mapDispatchToProps
 )(Counter)
