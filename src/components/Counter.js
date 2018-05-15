48f8ab604ef4c0ba92ed3d448646565d3e98d749
Pawel Wasil <wasil.paul@gmail.com>
2018-05-15 11:11:40 +0200

    only redux state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,12 +3,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { RaisedButton } from 'material-ui';
 
-
-
-export const Counter = (props) =>
+const Counter = (props) =>
     <div>
-        <h2> 0 </h2>
+        <h2> {props.counterValue} </h2>
         <RaisedButton label={'-'} primary={true} />
         <RaisedButton label={'+'} secondary={true} />
     </div>
 
+const mapStateToProps = state => ({
+    counterValue: state.counter
+}) 
+
+export default connect(
+    mapStateToProps,    
+    //
+)(Counter)
